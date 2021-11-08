f2574287e32fa85dd86b02bf00422e41837c2c88
sushidesu <hugemerge@gmail.com>
2021-11-08 21:14:52 +0900

    pretty log

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 import { spawnSync } from "child_process"
 
+const TRUN_PREFIX = `\x1b[32m[trun]\x1b[0m`
+
 function main() {
   let command = process.argv[2]
   if (!command) {
-    return
+     console.log(`${TRUN_PREFIX} \x1b[31merror\x1b[0m command not received`)
+     return
   }
   const args = process.argv.slice(3)
   for (const arg of args) {
     command = command.replace("{}", arg)
   }
-  console.log(`\x1b[32m[trun]\x1b[0m ${command}`)
+  console.log(`${TRUN_PREFIX} ${command}`)
   spawnSync(command, { shell: true, stdio: "inherit" })
 }
 
