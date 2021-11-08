b64df4af6da7410f7694606fb85c811d013066d8
sushidesu <hugemerge@gmail.com>
2021-11-08 21:07:19 +0900

    implement core

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@
 import { spawnSync } from "child_process"
 
 function main() {
-  spawnSync(`echo "hello!"`, { shell: true, stdio: "inherit" })
+  let command = process.argv[2]
+  if (!command) {
+    return
+  }
+  const args = process.argv.slice(3)
+  for (const arg of args) {
+    command = command.replace("{}", arg)
+  }
+  console.log(`\x1b[32m[trun]\x1b[0m ${command}`)
+  spawnSync(command, { shell: true, stdio: "inherit" })
 }
 
 main()
